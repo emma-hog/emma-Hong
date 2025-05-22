38aa9ee732a926b7ab5533acf4d72a71554f4f31
emma-hog <hongzang777@naver.com>
2025-05-22 12:41:48 +0900

    0522up

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
+document.addEventListener("DOMContentLoaded",function(){
 
+    const progressBar = document.querySelectorAll(".progress-bar")
 
-document.addEventListener("DOMContentLoaded", function(){
-
-    const progressBar = document.querySelectorAll(".progress-bar");
-  
     progressBar.forEach(bar => {
         const target= bar.getAttribute('data-bs-target')
         setTimeout(() =>{
@@ -12,5 +10,38 @@ document.addEventListener("DOMContentLoaded", function(){
     })
 
 
+    const searchInput = document.getElementById("searchInput")
+    const portfolioItems = document.querySelectorAll(".portfolio-item")
 
-})
+    searchInput.addEventListener("input", function() {
+        const searchTerm = this.value.toLowerCase();
+
+        portfolioItems.forEach((item)  => {
+            const title = item.querySelector("h4").textContent.toLowerCase();
+
+            if(title.includes(searchTerm)) {
+             item.style.display = "block";
+            } else {
+             item.style.display = "none";
+            }
+        })
+    })
+
+
+     const filterButtons=document.querySelectorAll(".filter-btn");
+
+
+
+    filterButtons.forEach((button) => {
+         button.addEventListener("click", function() {
+           const filter = this.getAttribute("data-filter");
+           portfolioItems.forEach((item) => {
+               if(filter === 'all' )  ||  item.getAttribute
+               ("data-category")  === filter) {
+                  item.style.display = "block" ;
+               } else {
+                  item.style.display = "none" ; 
+               }  
+            })
+        })
+})     
